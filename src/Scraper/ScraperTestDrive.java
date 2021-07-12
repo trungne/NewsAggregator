d8b3414fd9c0ce3f288889d0892038d8c3b1bfa0
trungne <nguyenquochoangtrung@gmail.com>
2021-07-12 15:52:23 +0700

    remove unnecessary imports

--- src/Scraper/ScraperTestDrive.java
+++ src/Scraper/ScraperTestDrive.java
@@ -1,17 +1,9 @@
 package Scraper;
 
 import News.Article;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.*;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.HashSet;
 
