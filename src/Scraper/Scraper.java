fb401ce3f1e3051fde736b9f1661e428df198288
trungne <nguyenquochoangtrung@gmail.com>
2021-07-16 13:36:04 +0700

    change method name for readability

--- src/Scraper/Scraper.java
+++ src/Scraper/Scraper.java
@@ -19,12 +19,14 @@ public class Scraper {
     public HashSet<Article> scrape(NewsOutlet newsOutlet){
         HashSet<Article> articles = new HashSet<>();
 
-        // step 1: get url to each category
-        // step 2: For each category, get all article links belong to that category
-        // step 3: For each article, create an article object
+        /*
+        step 1: get url to each category
+        step 2: For each category, get all article links belong to that category
+        step 3: For each article, create an article object
+        */
 
+        // step 1: get url to a specific category (value) by assessing the category name (key)
         for (String category: newsOutlet.categories.keySet()){
-            // step 1: get url to a specific category (value) by assessing the category name (key)
             URL urlToCategory;
             try{
                 urlToCategory = new URL(newsOutlet.categories.get(category));
@@ -35,7 +37,7 @@ public class Scraper {
             }
 
             // step 2
-            HashSet<URL> urlsInCategory = getAllArticleLinks(urlToCategory, newsOutlet.titleLinkCssClass);
+            HashSet<URL> urlsInCategory = getAllArticleLinksInCategory(urlToCategory, newsOutlet.titleLinkCssClass);
 
             // step 3
             for (URL url: urlsInCategory){
@@ -48,7 +50,7 @@ public class Scraper {
     }
 
     // get all article links in a category
-    private HashSet<URL> getAllArticleLinks(URL baseUrl, String titleLinkClass){
+    private HashSet<URL> getAllArticleLinksInCategory(URL baseUrl, String titleLinkClass){
         Document doc;
 
         // Do not store duplicates
