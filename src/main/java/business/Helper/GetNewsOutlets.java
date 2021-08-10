6c24941eb2042a76a75992b642fa52b4a4201b1c
trungne <nguyenquochoangtrung@gmail.com>
2021-08-10 17:31:08 +0700

    add static initialization for newsOutlets map

--- src/main/java/business/Helper/GetNewsOutlets.java
+++ src/main/java/business/Helper/GetNewsOutlets.java
@@ -6,15 +6,13 @@ import business.NewsSources.*;
 import java.util.HashMap;
 
 public class GetNewsOutlets {
-    public static final HashMap<String, NewsOutlet> newsOutlets = initializeNewsOutlets();
-
-    public static HashMap<String, NewsOutlet> initializeNewsOutlets() {
-        HashMap<String, NewsOutlet> newsList = new HashMap<>();
-        newsList.put("VNExpress", VNExpress.init());
-        newsList.put("ZingNews", ZingNews.init());
-        newsList.put("ThanhNien", ThanhNien.init());
-        newsList.put("TuoiTre", TuoiTre.init());
-        newsList.put("NhanDan", NhanDan.init());
-        return newsList;
+    public static final HashMap<String, NewsOutlet> newsOutlets = new HashMap<>();
+    static {
+        newsOutlets.put("VNExpress", VNExpress.init());
+        newsOutlets.put("ZingNews", ZingNews.init());
+        newsOutlets.put("ThanhNien", ThanhNien.init());
+        newsOutlets.put("TuoiTre", TuoiTre.init());
+        newsOutlets.put("NhanDan", NhanDan.init());
     }
+
 }
